0b4a863bd6bfce7c3df72ed23a3fbea71fa9d4b2
keer2345 <keer2345@gmail.com>
2020-07-29 22:48:52 +0800

    ch03 worker.ex

---
+++ ch03/metex/lib/worker.ex
@@ -0,0 +1,37 @@
+defmodule Metex.Worker do
+  def temperature_of(location) do 
+    result = url_for(location) |> HTTPoison.get |> parse_response
+    case result do
+      {:ok, temp} -> "#{location}: #{temp}°C"
+      :error -> "#{location} not found"
+    end
+  end
+
+  defp url_for(location) do
+    location = URI.encode(location)
+    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{apikey}"
+  end
+
+  defp parse_response(resp) do
+    case resp do
+      {:ok, %HTTPoison.Response{body: body, status_code: 200}} -> 
+        body |> JSON.decode |> compute_temperature
+      _ -> :error
+    end
+  end
+
+  defp compute_temperature(json) do
+    try do
+      {:ok, json} = json
+      temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
+      {:ok, temp}
+    rescue
+      _ -> :error
+    end
+  end
+
+  defp apikey do
+    "dc4408560f0839350a00ed3420aa7445"
+  end
+
+end
